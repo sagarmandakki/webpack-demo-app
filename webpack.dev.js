e0d457642b18a95c89d179f716f97f2935c91716
Sagar <sagar@Sagars-MacBook-Pro.local>
2020-06-22 02:39:33 +0530

    webpack understood!, almost.

---
+++ webpack.dev.js
@@ -0,0 +1,29 @@
+const path = require('path')
+const common = require('./webpack.common')
+const merge = require('webpack-merge')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = merge(common, {
+  mode: 'development',
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader', // 3
+          'css-loader', // 2
+          'sass-loader' // 1
+        ]
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/template.html',
+    })
+  ]
+})
